4ff1820f85dc9021c0503255154b286ca454c856
Onas55 <Onas54@icloud.com>
2024-08-14 03:38:57 -0700

    QR CODE

---
+++ PYTHON QR CODE.py
@@ -0,0 +1,22 @@
+import qrcode
+
+data = "Hello, World!"
+
+qr = qrcode.QRCode(
+    version=1,  
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  
+    box_size=10,  
+    border=4,  
+)
+
+
+qr.add_data(data)
+qr.make(fit=True)
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+
+img.save("qrcode.png")
+
+print("QR code generated and saved as 'qrcode.png'")
